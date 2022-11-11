7206c121f8ae78fcfe9c8cd3ad7a5db388aad1f5
Günther Rezniczek <g.rezniczek@gmail.com>
2022-11-11 17:00:58 +0100

    Parse AT

--- REDCapScrambleExternalModule.php
+++ REDCapScrambleExternalModule.php
@@ -1,11 +1,15 @@
 <?php namespace RUB\REDCapScrambleExternalModule;
 
 require_once "classes/InjectionHelper.php";
+require_once "classes/ActionTagParser.php";
 
 /**
  * ExternalModule class for REDCap Scramble.
  */
 class REDCapScrambleExternalModule extends \ExternalModules\AbstractExternalModule {
+
+
+    const AT_SCRAMBLE = "@SCRAMBLE";
 
     #region Constructor and Instance Variables
 
@@ -13,9 +17,16 @@ class REDCapScrambleExternalModule extends \ExternalModules\AbstractExternalModu
      * @var InjectionHelper
      */
     public $ih = null;
+
+    /**
+     * EM Framework (tooling support)
+     * @var \ExternalModules\Framework
+     */
+    private $fw;
  
     function __construct() {
         parent::__construct();
+        $this->fw = $this->framework;
         $this->ih = InjectionHelper::init($this);
     }
 
@@ -31,9 +42,40 @@ class REDCapScrambleExternalModule extends \ExternalModules\AbstractExternalModu
 
     function redcap_survey_page($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id = NULL, $repeat_instance = 1) {
 
+        $tags = $this->get_scramble_settings($project_id, $instrument, $event_id, $repeat_instance);
+
         $this->ih->js("js/redcap_scramble.js", true);
+        print "<script>REDCap.EM.RUB.REDCapScramble.init(".json_encode($tags, JSON_UNESCAPED_UNICODE).");</script>";
     }
 
     #endregion
+
+
+    private function get_scramble_settings($pid, $form, $event_id, $instance) {
+        $tags = [];
+        $Proj = new \Project($pid);
+
+        foreach ($Proj->forms[$form]["fields"] as $field => $_) {
+            $meta = $Proj->metadata[$field];
+            $misc = $meta["misc"] ?? "";
+            if (strpos($misc, self::AT_SCRAMBLE) !== false) {
+                $result = ActionTagParser::parse($misc);
+                foreach ($result["parts"] as $at) {
+                    if ($at["text"] == self::AT_SCRAMBLE && $at["param"]["type"] == "quoted-string") {
+                        $tags[$field] = array_map(function($s) { return trim($s); }, explode(",",trim($at["param"]["text"],"\"")));
+                    }
+                }
+            }
+        }
+        foreach ($tags as $field => &$fields) {
+            // Generate random order
+            $sort_by = [];
+            while (count($sort_by) < count($fields)) {
+                $sort_by[] = random_int(PHP_INT_MIN, PHP_INT_MAX);
+            }
+            array_multisort($sort_by, SORT_NUMERIC, $fields);
+        }
+        return $tags;
+    }
 
 }
--- js/redcap_scramble.js
+++ js/redcap_scramble.js
@@ -1,1 +1,37 @@
-console.log('Scrambled');
+
+/**
+ * REDCap Scramble
+ */
+// @ts-check
+;(function() {
+
+//#region Variables and Initialization
+
+if (typeof window['REDCap'] == 'undefined') {
+    window['REDCap'] = {
+        EM: {}
+    };
+}
+if (typeof window['REDCap']['EM'] == 'undefined') {
+    window['REDCap']['EM'] = {
+        RUB: {}
+    };
+}
+if (typeof window['REDCap']['EM']['RUB'] == 'undefined') {
+    window['REDCap']['EM']['RUB'] = {};
+}
+window['REDCap']['EM']['RUB']['REDCapScramble'] = {
+    init: init,
+};
+
+let config;
+
+function init(data) {
+    config = data;
+    console.log('Scrambled', config);
+}
+
+//#endregion
+
+
+})();
